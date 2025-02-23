2a0cafda445ab1210e96f6772ccd6db32fab3217
AbduLLa <58235388+AbduLLa-proger@users.noreply.github.com>
2025-02-23 14:05:25 +0500

    added base configuration of project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+export const App = () => (
+  <Provider store={store}
+)
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/views/components/'),
+      '@features': path.resolve(__dirname, 'src/features/'),
+      '@pages': path.resolve(__dirname, 'src/views/pages/'),
+      '@services': path.resolve(__dirname, 'src/services/'),
+      '@utils': path.resolve(__dirname, 'src/utils/'),
+    }
+  }
+})
